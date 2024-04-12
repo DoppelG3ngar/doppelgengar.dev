3ad04ddbf14535d9f7f4af3de8864e150714b112
Kyle Moreira <Kyle.moreira@doppelgengar.dev>
2024-04-12 10:48:38 -0400

    Create bio (#8)
    * style: Move components into its own directory
    
    * update directory name
    
    * reset
    
    * reset
    
    * Rework home page
    
    * Adjust the forground color
    
    * Adjusting action to remove color formatting
    
    * Adding a bio
    
    * Adjusting responsivness

--- doppelgengar.dev/app/page.tsx
+++ doppelgengar.dev/app/page.tsx
@@ -1,10 +1,9 @@
 export default function Home() {
   return (
-    <p className="absolute bottom-7 right-7 m-3 w-1/3 text-white">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae animi
-      dolorem obcaecati fugiat odit quis sit non velit, minima sapiente. Numquam
-      minima ullam doloribus necessitatibus aspernatur expedita modi consectetur
-      consequatur?
+    <p className="absolute bottom-7 right-7 m-3 md:m-10 w-1/3 text-white md:w-1/4">
+      I&#39;m Kyle, a developer diving headfirst into the world of DevOps.
+      I&#39;m all about pushing boundaries, automating processes, and optimizing
+      workflows to create seamless, efficient systems.
     </p>
   );
 }
