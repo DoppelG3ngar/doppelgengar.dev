f49b85786904817788410bf5b2473e536ed75adf
DoppelG3ngar <kyle.moreira@doppelgengar.dev>
2024-04-12 09:38:01 -0400

    Rework home page

--- doppelgengar.dev/app/layout.tsx
+++ doppelgengar.dev/app/layout.tsx
@@ -1,8 +1,10 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
+import '@/app/ui/styles/globals.css';
+import NavHeader from './ui/navigation/nav-header';
 
 export const metadata: Metadata = {
-  title: "Kyle Moreira",
-  description: "A portfolio for Kyle Moreira",
+  title: 'Kyle Moreira Portfolio',
+  description: 'A portfolio for Kyle Moreira',
 };
 
 export default function RootLayout({
@@ -12,7 +14,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body >{children}</body>
+      <body className="h-screen bg-background px-6 py-5 ">
+        <div className="h-full bg-foreground overflow-hidden border p-3">
+          <header className='fixed'>
+            <NavHeader />
+          </header>
+          <main>{children}</main>
+        </div>
+      </body>
     </html>
   );
 }
---
+++ doppelgengar.dev/app/ui/navigation/nav-link.tsx
@@ -0,0 +1,38 @@
+'use client';
+import clsx from 'clsx';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+
+// Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
+const links = [
+  { name: 'home', href: '/' },
+  { name: 'work', href: '/work' },
+  { name: 'contact', href: '/contact' },
+  { name: 'blog', href: '/blog' },
+];
+
+export default function NavLinks() {
+  const pathname = usePathname();
+  return (
+    <>
+      {links.map((link) => {
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={clsx('text-shade', {
+              'font-bold text-white': pathname === link.href,
+            })}
+          >
+            <p>
+              <span className="text-accent">#</span>
+              {link.name}
+            </p>
+          </Link>
+        );
+      })}
+    </>
+  );
+}
