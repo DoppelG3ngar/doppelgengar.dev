b4b34199910b620b745dd7352ea86d16b2d5990a
DoppelG3ngar <kyle.moreira@doppelgengar.dev>
2024-04-12 10:42:15 -0400

    Adjusting responsivness

--- doppelgengar.dev/app/ui/navigation/nav-header.tsx
+++ doppelgengar.dev/app/ui/navigation/nav-header.tsx
@@ -2,9 +2,9 @@ import NavLinks from './nav-link';
 
 export default function NavHeader() {
   return (
-    <nav>
-      <h1 className="text-3xl text-white">Kyle Moreira</h1>
-      <p className="text-sm text-white">Developer</p>
+    <nav className='md:pt-5 md:pl-5 '>
+      <h1 className="text-3xl md:text-5xl text-white">Kyle Moreira</h1>
+      <p className="text-sm md:text-lg text-white">Developer</p>
       <div className="pt-10">
         <NavLinks />
       </div>
